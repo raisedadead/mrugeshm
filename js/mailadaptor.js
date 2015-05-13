6737ce08e011cb7aa71f031faf34e32be2015a36
Mrugesh Mohapatra <me@raisedadead.com>
2015-05-14 00:25:00 +0530

    fix firstName in error message

--- js/mailadaptor.js
+++ js/mailadaptor.js
@@ -42,7 +42,7 @@ var initMailApp = function( $form , data ) {
         $('#success').html("<div class='alert alert-danger'>");
         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-        $('#success > .alert-danger').append("<strong>Sorry " + this.name.val() + ", it seems that my mail server is not responding. Please try again later!");
+        $('#success > .alert-danger').append("<strong>Sorry " + data.firstName + ", it seems that my mail server is not responding. Please try again later!");
         $('#success > .alert-danger').append('</div>');
         //clear all fields
         $('#contactForm').trigger("reset");
